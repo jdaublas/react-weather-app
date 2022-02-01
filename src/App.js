49823a5f359614da7b30b73f5ff547661746b8ca
jdaublas <87877758+jdaublas@users.noreply.github.com>
2022-02-01 14:32:54 -0700

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Weather app</h1>
-      <p>
-        Coded by Joveth Daublas and is {""}
-        <a href="https://github.com/jdaublas/react-weather-app">
-          Open-sourced:github
-        </a>
-      </p>
+    <div className="App">
+      <div className="Container">
+        <button className="btn btn-primary">Hello</button>
+        <h1>Weather app</h1>
+        <footer>
+          Coded by Joveth Daublas and is {""}
+          <a href="https://github.com/jdaublas/react-weather-app">
+            Open-sourced:github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
