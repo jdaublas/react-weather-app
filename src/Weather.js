ff95f82c8979695befb276eef62ce779ffbeff7e
jdaublas <87877758+jdaublas@users.noreply.github.com>
2022-02-01 17:31:37 -0700

    updated format

--- src/Weather.js
+++ src/Weather.js
@@ -10,30 +10,26 @@ export default function Weather() {
           <input type="Submit" value="Search" className="btn btn-primary" />
         </form>
       </div>
-
       <div className="row">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="mostly_sunny"
           />
-          <div className="Temp">-17°C</div>
+          <div className="Temp">-17</div>
+          <div className="Degrees">°C|F</div>
           <div className="Description">
             <ul>
               <li>Precipitation: 2%</li>
-
               <li>Humidity: 63%</li>
-
               <li>Wind: 8 km/h</li>
             </ul>
           </div>
         </div>
-
         <div className="col-6">
           <div className="City">
             <h1>Edmonton, AB</h1>
           </div>
-
           <div className="City">
             <ul>
               <li>Tuesday 2:00 p.m.</li>
