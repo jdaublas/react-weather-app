42595bb7f07a7123b5a3d0190ccae2dea1f5ce20
jdaublas <87877758+jdaublas@users.noreply.github.com>
2022-02-04 15:42:47 -0700

    added components

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./Forecast.css";
 import axios from "axios";
 
 export default function Forecast(props) {
-  function handleResponse(response) {
-    console.log(response.data);
-  }
-  let apiKey = "7cc4ba5a8fa8fdc11de61ff161ff458d";
-  let lat = props.coords;
-  let lon = props.coords;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
 
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col">
-          Thu
-          <div>
-            <WeatherIcon code="01d" size={24} />
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <div className="col">
+            <div>Thu</div>
+            <div>
+              <WeatherIcon code="01d" size={24} />
+            </div>
+            <span className="Forecast-max">19°</span>
+            <span className="Forecast-min">16°</span>
           </div>
-          <span className="Forecast-max">19°</span>
-          <span className="Forecast-min">16°</span>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "7cc4ba5a8fa8fdc11de61ff161ff458d";
+    let lat = props.coords;
+    let lon = props.coords;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
